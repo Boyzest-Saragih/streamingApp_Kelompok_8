2fbf7e45c56654bcc8967db90902edd11fc65a44
Boyzest-Saragih <boyzestsumbayak@gmail.com>
2025-07-10 23:19:13 +0700

    kelas

--- lib/screens/favoriteScreen.dart
+++ lib/screens/favoriteScreen.dart
@@ -22,95 +22,93 @@ class _FavoritescreenState extends State<Favoritescreen> {
     final languageProvider = Provider.of<LanguageProv>(context);
     final enLang = languageProvider.currentLanguage == "en";
 
-    print('FavoriteScreen - Current language: ${languageProvider.currentLanguage}');
-    print('FavoriteScreen - enLang: $enLang');
-
     final favorite = favoriteMovies.getFavorites(user!.userId.toString());
 
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child:
-            favorite.isEmpty
-                ? const Center(
-                  child: Text(
-                    "No favorite movies yet.",
-                    style: TextStyle(fontSize: 16),
-                  ),
-                )
-                : GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    mainAxisSpacing: 10,
-                    crossAxisSpacing: 10,
-                    childAspectRatio: 0.8,
-                  ),
-                  itemCount: favorite.length,
-                  itemBuilder: (context, index) {
-                    final movie = favorite[index];
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder:
-                                (context) => WatchPage(idMovie: movie.movieId),
-                          ),
-                        );
-                      },
-                      child: Container(
-                        width: 150,
-                        margin: EdgeInsets.symmetric(horizontal: 8),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                            image: NetworkImage(
-                              getImageUrl(movie.posterPath),
-                            ),
-                            fit: BoxFit.cover,
-                          ),
+        child: favorite.isEmpty
+            ? Center(
+                child: Text(
+                  enLang
+                      ? "No favorite movies yet."
+                      : "Belum ada film favorit.",
+                  style: const TextStyle(fontSize: 16),
+                ),
+              )
+            : GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  childAspectRatio: 0.8,
+                ),
+                itemCount: favorite.length,
+                itemBuilder: (context, index) {
+                  final movie = favorite[index];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              WatchPage(idMovie: movie.movieId),
                         ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Container(
-                              alignment: Alignment.bottomCenter,
-                              height: 48,
-                              decoration: BoxDecoration(
-                                gradient: LinearGradient(
-                                  begin: Alignment.bottomCenter,
-                                  end: Alignment.topCenter,
-                                  colors: [Colors.black, Colors.transparent],
+                      );
+                    },
+                    child: Container(
+                      width: 150,
+                      margin: const EdgeInsets.symmetric(horizontal: 8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            getImageUrl(movie.posterPath),
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Container(
+                            alignment: Alignment.bottomCenter,
+                            height: 48,
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                begin: Alignment.bottomCenter,
+                                end: Alignment.topCenter,
+                                colors: [Colors.black, Colors.transparent],
+                              ),
+                            ),
+                            child: Column(
+                              children: [
+                                Text(
+                                  movie.title,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(color: Colors.white),
                                 ),
-                              ),
-                              child: Column(
-                                children: [
-                                  Text(
-                                    movie.title,
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      const Icon(
-                                        Icons.star,
-                                        color: Colors.yellow,
-                                        size: 16,
-                                      ),
-                                      const SizedBox(width: 4),
-                                      Text(
-                                        movie.rating
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Icon(
+                                      Icons.star,
+                                      color: Colors.yellow,
+                                      size: 16,
+                                    ),
+                                    const SizedBox(width: 4),
+                                    Text(
+                                      movie.rating,
+                                      style: const TextStyle(color: Colors.white),
+                                    ),
+                                  ],
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   );
